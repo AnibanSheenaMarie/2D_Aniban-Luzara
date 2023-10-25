62431dfe68997d6ca2549a4aa8aad1257302391a
Sheena Marie Aniban <116483466+AnibanSheenaMarie@users.noreply.github.com>
2023-10-25 20:12:19 +0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -3,21 +3,23 @@ private string title;
 private int numberofPages;
 private boolean is fictional;
 
-public String getTitle(){
-return title;
-}
-public String getnum_pages(){
-return num_pages;
-}
-public boolean getIs_fiction(){
-return is_fiction;
-}
+ public String getTitle(){
+ return title;
+ }
+ public String getnum_pages(){
+ return num_pages;
+ }
+ public boolean getIs_fiction(){
+ return is_fiction;
+ }
 
-public void setTitle(){
-this.title;
+ public void setTitle(){
+ this.title;
+ }
+ public void setgetnumofpages(){
+ this.getnumofpages;
+ }
+ public boolean setIs_fiction(){
+ this.boolean is fiction;
+ }
 }
-public void setgetnumofpages(){
-this.getnumofpages;
-}
-public boolean setIs_fiction(){
-this.boolean is fiction;
